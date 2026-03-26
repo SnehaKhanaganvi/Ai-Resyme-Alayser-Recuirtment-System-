00c3bb747088aac058b4ed1c39ad88a6052bb520
S P Darshan <117818227+spdarshan46@users.noreply.github.com>
2026-03-26 13:15:20 +0530

    Delete backend directory

--- backend/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { protect } = require('../middleware/authMiddleware');
-const { sendWhatsAppMessage } = require('../controllers/notificationController');
-const router = express.Router();
-
-router.post('/whatsapp', protect, sendWhatsAppMessage);
-
-module.exports = router;
--- backend/utils/whatsappService.js
+++
@@ -1,7 +0,0 @@
-// utils/whatsappService.js
-const sendWhatsApp = async (phone, message) => {
-  console.log(`📱 Sending WhatsApp to ${phone}: ${message}`);
-  return true;
-};
-
-module.exports = { sendWhatsApp };
